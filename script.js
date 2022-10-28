6b514c8bd08cd610b13386948fd7c9ef086aa8b2
Honzi07 <nyisztor.zsolt@gmail.com>
2022-10-28 06:05:26 +0200

    added clear functions

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ const btnSubtract = document.querySelector('.subtract');
 const btnMultiply = document.querySelector('.multiply');
 const btnDivide = document.querySelector('.divide');
 const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const clearEntry = document.querySelector('.clear-entry');
+const clearLast = document.querySelector('.clear-last');
 
 
-let display;
+
+let display = '';
 const storage = [];
 let newArr = [];
-console.log(display);
+
 
 function displayContent() {
     inputDisplay.textContent = display;
@@ -23,7 +27,7 @@ numbers.forEach((number) => {
     number.addEventListener('click', (e) => {
         storage.push(e.target.textContent);
         display = storage.join('');
-        console.log(display);
+        console.log(storage);
     });
 });
 
@@ -33,7 +37,6 @@ operators.forEach((operator) => {
         display = e.target.textContent;
         newArr.push(e.target.textContent);
         storage.length = 0;
-        console.log({display});
         console.log(storage);
     });
 });
@@ -45,30 +48,53 @@ equal.addEventListener('click', () => {
     newArr.push(display);
 });
 
-function test() {
-    for(let i = 0; i < newArr.length; i++) {
-        switch (newArr[i]) {
-            case '+':
-                display = newArr[0] + parseFloat(display);
-                break;
-            case '-':
-                display = newArr[0] - parseFloat(display);
-                break;
-            case '÷':
-                display = newArr[0] / parseFloat(display);
-                break;
-            case 'x':
-                display = newArr[0] * parseFloat(display);
-                break;
-        } 
-    }
+// function test() {
+//     for(let i = 0; i < newArr.length; i++) {
+//         switch (newArr[i]) {
+//             case '+':
+//                 display = newArr[0] + parseFloat(display);
+//                 break;
+//             case '-':
+//                 display = newArr[0] - parseFloat(display);
+//                 break;
+//             case '÷':
+//                 display = newArr[0] / parseFloat(display);
+//                 break;
+//             case 'x':
+//                 display = newArr[0] * parseFloat(display);
+//                 break;
+//         } 
+//     }
 
+// };
+
+
+function test() {
+    if(newArr.includes('+')) {
+        display = newArr[0] + parseFloat(display);
+    }
 };
+
+
+clear.addEventListener('click', () => {
+    display = 0;
+    storage.length = 0;
+    newArr.length = 0;
+});
+
+clearEntry.addEventListener('click', () => {
+    display = 0;
+});
+
+clearLast.addEventListener('click', () => {
+    storage.pop();
+    display = storage.join('');
+});
 
 
 
 document.addEventListener('click', () => {
     displayContent();
-
+    console.log(newArr)
 });
 
