7f01bd9af5716b4f0e05b87e3563ce38e962b749
Honzi07 <nyisztor.zsolt@gmail.com>
2022-11-14 20:07:21 +0100

    changes in checkOperator, subtract, disableEqual functions changed disableDot function to a shorter version  added button for input negative numbers

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const btnSubtract = document.querySelector('.subtract');
 const btnMultiply = document.querySelector('.multiply');
 const btnDivide = document.querySelector('.divide');
 const btnDot = document.querySelector('.dot');
+const btnPlusMinus = document.querySelector('.plusMinus');
 const equal = document.querySelector('.equal');
 const clear = document.querySelector('.clear');
 const clearEntry = document.querySelector('.clear-entry');
@@ -44,9 +45,27 @@ btnDot.addEventListener('click', () => {
     display += btnDot.textContent;
 });
 
+btnPlusMinus.addEventListener('click',() => {
+    plusMinus();
+});
+
+function plusMinus() {
+    const op = ['x','-','÷','+'];
+    if(display >= 1 && !isNaN(display[0])) {
+        display = `-${display}`;
+        console.log('1')
+    } else if(op.indexOf(display) != -1 || (typeof parseFloat(display[1]) === 'number' && display.length >= 2 && display[0] != '-' && display[1] != '-')) {
+        display = (display.substring(0, 1) + '-' + display.substring(1))
+        console.log('2')
+    } else {
+        display = display.replace(/-/, '');
+        console.log('3')
+    }
+};
+
 function checkOperator() {
-    for(let i = 0; i < display.length; i++) {
-        switch(display[i]) {
+    if(typeof display[0] === 'string' && typeof parseFloat(display[1]) === 'number' && display.length >= 2) {
+        switch(display[0]) {
             case '+':
                 add();
                 break;
@@ -71,10 +90,14 @@ function add() {
 };
 
 function subtract() {
-    display = display.substring(1, display.length)
-    display = storage[0] - parseFloat(display);
-    display = parseFloat(display.toFixed(8));
-    storage.unshift(display);
+    if(storage.length === 0) {
+        storage.push(parseFloat(display))
+    } else {
+        display = display.substring(1, display.length)
+        display = storage[0] - parseFloat(display);
+        display = parseFloat(display.toFixed(8));
+        storage.unshift(display);
+    }
 };
 
 function divide() {
@@ -98,10 +121,23 @@ function replace0() {
 };
 
 function disableDot() {
-    let ans = display.match(/\./g) ? true : false;
-    if(ans === true) {
+    // let ans = display.match(/\./g) ? true : false;
+    // if(ans === true) {
+    //     btnDot.style.pointerEvents = 'none';
+    // } else  btnDot.style.removeProperty('pointer-events');
+
+    if(display.match(/\./g)) {
         btnDot.style.pointerEvents = 'none';
     } else  btnDot.style.removeProperty('pointer-events');
+};
+
+function disableEqual() {
+    const op = ['x','-','÷','+'];
+        if((op.indexOf(display) != -1 && display.length === 1)
+        || (op.indexOf(display) === -1 && display[1] === '.' && display.length <= 3) 
+        || ((op.indexOf(display[0]) > -1 && op.indexOf(display[1]) > -1) && display.length <= 2)) {
+            equal.style.pointerEvents = 'none';
+        } else  equal.style.removeProperty('pointer-events');
 };
 
 equal.addEventListener('click', () => {
@@ -131,11 +167,3 @@ document.addEventListener('click', () => {
     disableEqual();
     console.log(storage);
 });
-
-function disableEqual() {
-    const op = ['x','-','÷','+'];
-        if((op.indexOf(display) != -1 && display.length === 1) 
-        || (op.indexOf(display) === -1 && display[1] === '.' && display.length <= 2)) {
-            equal.style.pointerEvents = 'none';
-        } else  equal.style.removeProperty('pointer-events');
-};
